453b23f5645018feccd5db985e4a2e2dbca31fe3
noahesuter <noahesuter@gmail.com>
2023-05-19 00:55:23 +0200

    moving all the chunks that do not represent results into a .R file for better code management

--- functions/large_files_ex_6.R
+++ functions/large_files_ex_6.R
@@ -1,3 +1,4 @@
+### Reading and Cleaning the Davos dataset
 
 library(lubridate)
 library(dplyr)
@@ -31,6 +32,212 @@ FLX_Dav <- readr::read_csv("../data/FLX_CH-Dav_FLUXNET2015_FULLSET_DD_1997-2014_
   
   # drop QC variables (no longer needed)
   dplyr::select(-ends_with("_QC"))
+
+
+
+### Reading and Cleaning the Laegern dataset
+
+
+library(lubridate)
+library(dplyr)
+FLX_Lae <- readr::read_csv("../data/FLX_CH-Lae_FLUXNET2015_FULLSET_DD_2004-2014_1-4.csv") |> # reading the data for Laegern into R   
+  
+  # select only the variables we are interested in
+  dplyr::select(TIMESTAMP,
+                GPP_NT_VUT_REF,    # the target
+                ends_with("_QC"),  # quality control info
+                ends_with("_F"),   # includes all all meteorological covariates
+                -contains("JSB")   # weird useless variable
+  ) |>
+  
+  # convert to a nice date object
+  dplyr::mutate(TIMESTAMP = lubridate::ymd(TIMESTAMP)) |>
+  
+  # set all -9999 to NA
+  mutate(across(where(is.numeric), ~na_if(., -9999))) |> #deleting all values -9999
+  
+  
+  # retain only data based on >=80% good-quality measurements
+  # overwrite bad data with NA (not dropping rows)
+  dplyr::mutate(GPP_NT_VUT_REF = ifelse(NEE_VUT_REF_QC < 0.8, NA, GPP_NT_VUT_REF),
+                TA_F           = ifelse(TA_F_QC        < 0.8, NA, TA_F),
+                SW_IN_F        = ifelse(SW_IN_F_QC     < 0.8, NA, SW_IN_F),
+                LW_IN_F        = ifelse(LW_IN_F_QC     < 0.8, NA, LW_IN_F),
+                VPD_F          = ifelse(VPD_F_QC       < 0.8, NA, VPD_F),
+                PA_F           = ifelse(PA_F_QC        < 0.8, NA, PA_F),
+                P_F            = ifelse(P_F_QC         < 0.8, NA, P_F),
+                WS_F           = ifelse(WS_F_QC        < 0.8, NA, WS_F)) |> 
+  
+  # drop QC variables (no longer needed)
+  dplyr::select(-ends_with("_QC"))
+
+
+### Splitting the data
+
+
+# Data splitting for Dav with a 80/20 split
+set.seed(1982)  # made for reproducibility
+split <- rsample::initial_split(FLX_Dav, prop = 0.8)
+FLX_Dav_train<- rsample::training(split)
+FLX_Dav_test <- rsample::testing(split)
+
+
+
+# Data splitting for Lae with a 80/20 split
+set.seed(1982)  # made for reproducibility
+split <- rsample::initial_split(FLX_Lae, prop = 0.8)
+FLX_Lae_train<- rsample::training(split)
+FLX_Lae_test <- rsample::testing(split)
+
+
+### Deleting the column LW_IN_F
+
+
+FLX_Dav_test$LW_IN_F <- NULL #deleting the column LW_IN_F in Davos test set
+
+
+
+FLX_Dav_train$LW_IN_F <- NULL #deleting the column LW_IN_F in Davos train set
+
+
+FLX_Lae_test$LW_IN_F <- NULL #deleting the column LW_IN_F in Laegern test set
+
+
+FLX_Lae_train$LW_IN_F <- NULL #deleting the column LW_IN_F in Laegern train test
+
+
+### Deleting the column P_F 
+
+FLX_Lae_test$P_F <- NULL #deleting the column P_F in Laegern test set
+
+
+FLX_Lae_train$P_F <- NULL #deleting the column P_F in Laegern train set
+
+
+FLX_Dav_test$P_F <- NULL #deleting the column P_F in Davos test set
+
+
+FLX_Dav_train$P_F <- NULL #deleting the column P_F in Davos train set
+
+### Deleting the rows containing missing values
+
+FLX_Dav_test<- stats::na.omit(FLX_Dav_test) #deleting all the rows with missing values in Davos test set
+
+
+FLX_Dav_train<- stats::na.omit(FLX_Dav_train) #deleting all the rows with missing values in Davos train set
+
+
+
+FLX_Lae_test<- stats::na.omit(FLX_Lae_test) #deleting all the rows with missing values in Laegern test set
+
+
+
+FLX_Lae_train<- stats::na.omit(FLX_Lae_train) #deleting all the rows with missing values in Laegern train set
+
+
+### Defining the recipes of the models for the train datasets (Davos and Laegern)
+
+
+model_dav_train <- recipes::recipe(GPP_NT_VUT_REF ~ SW_IN_F + VPD_F + PA_F + WS_F + TA_F, 
+                                   data = FLX_Dav_train) |> #recipe for the model of the Davos dataset
+  recipes::step_center(all_numeric(), -all_outcomes()) |>
+  recipes::step_scale(all_numeric(), -all_outcomes())
+
+
+model_Lae_train <- recipes::recipe(GPP_NT_VUT_REF ~ SW_IN_F + VPD_F + PA_F + WS_F + TA_F, 
+                                   data = FLX_Lae_train) |> #recipe for the model of the Davos dataset
+  recipes::step_center(all_numeric(), -all_outcomes()) |>
+  recipes::step_scale(all_numeric(), -all_outcomes())
+
+
+
+
+
+### Using the caret function to determine the best model with maschine learning for Davos
+
+# training the best knn model with the Davos train data
+library(tidyverse)
+library(recipes)
+set.seed(1982)
+caret_dav_train_knn <- caret::train(model_dav_train, 
+                                    data = FLX_Dav_train |> drop_na(), 
+                                    method = "knn",
+                                    trControl = caret::trainControl(method = "cv", number = 10),
+                                    tuneGrid = data.frame(k = c(2, 5, 10, 15, 20, 25, 30, 35, 40, 60, 100)),
+                                    metric = "MAE")
+
+
+
+# training the best lm model with the Davos train data
+caret_dav_train_lm <- caret::train(model_dav_train, 
+                                   data = FLX_Dav_train |> drop_na(),  # drop missing values
+                                   trControl = caret::trainControl(method = "none"), # no resampling
+                                   method = "lm"
+)
+
+
+
+### Using the caret function to determine the best model with maschine learning for Laegern   
+
+
+# training the best knn model with the Laegern train data
+library(tidyverse)
+library(recipes)
+set.seed(1982)
+caret_Lae_train_knn <- caret::train(model_Lae_train, 
+                                    data = FLX_Lae_train |> drop_na(), 
+                                    method = "knn",
+                                    trControl = caret::trainControl(method = "cv", number = 10),
+                                    tuneGrid = data.frame(k = c(2, 5, 10, 15, 20, 25, 30, 35, 40, 60, 100)),
+                                    metric = "MAE")
+
+
+
+
+# training the best lm model with the Laegern train data
+caret_Lae_train_lm <- caret::train(model_Lae_train, 
+                                   data = FLX_Lae_train |> drop_na(),  # drop missing values
+                                   trControl = caret::trainControl(method = "none"), # no resampling
+                                   method = "lm"
+)
+
+
+
+
+
+### Pooling the dataframes together (each one for train and test)
+
+
+library(base)
+bind_FLX_train <- rbind(FLX_Lae_train, FLX_Dav_train)
+
+
+
+library(base)
+bind_FLX_test <- rbind(FLX_Lae_test, FLX_Dav_test)
+
+
+
+### Building a recipe for the pooled model
+
+model_combined_train <- recipes::recipe(GPP_NT_VUT_REF ~ SW_IN_F + VPD_F + PA_F + WS_F + TA_F, 
+                                        data = bind_FLX_train) |> #recipe for the model of the Davos dataset
+  recipes::step_center(all_numeric(), -all_outcomes()) |>
+  recipes::step_scale(all_numeric(), -all_outcomes())
+
+
+# training the best knn model with the pooled train data
+library(tidyverse)
+library(recipes)
+set.seed(1982)
+caret_combined_train_knn <- caret::train(model_combined_train, 
+                                         data = bind_FLX_train |> drop_na(), 
+                                         method = "knn",
+                                         trControl = caret::trainControl(method = "cv", number = 10),
+                                         tuneGrid = data.frame(k = c(2, 5, 10, 15, 20, 25, 30, 35, 40, 60, 100)),
+                                         metric = "MAE")
+
+
 
 
 # make model evaluation into a function to reuse code
@@ -95,7 +302,6 @@ eval_model <- function(mod, df_train, df_test){
   
   return(out)
 }
-
 
 
 
